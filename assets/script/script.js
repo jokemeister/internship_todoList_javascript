54ab6de643f63b8e006a3dda4e9161819cb99157
Daniil Dubrovskyi <ddi.dubrovskyi@gmail.com>
2022-08-23 17:27:18 +0300

    DOM done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,50 +1,95 @@
-const toDoList = [
-    {name: 'Увімкнути тахометр', description: 'Інакше буде боляче', done: true, due_date: '2022-08-22', list_id: 1},
-    {name: 'Доробити верстку макету', done: false, due_date: '2022-08-20', list_id: 1},
-    {name: 'Піти додому', description: 'Інакше мене закриють у офісі', done: false, due_date: '2022-08-22', list_id: 1}
-]
+const db = [
+    {id: 1, name: 'Увімкнути тахометр', description: 'Інакше буде боляче', done: true, due_date: '2022-08-22', list_id: 1},
+    {id: 2, name: 'Доробити верстку макету', done: false, due_date: '2022-08-20', list_id: 1},
+    {id: 3, name: 'Піти додому', description: 'Інакше мене закриють у офісі', done: false, due_date: '2022-08-23', list_id: 1},
+];
 
-const tasksBlock = document.querySelector('.js--hero__block');
-const taskTemplate = document.querySelector('.js--task__template');
+// default markup elements
+    const navBar = document.querySelector('.header__nav');
+    const tasksBlock = document.querySelector('.content');
+    const taskTemplate = document.querySelector('.task-template');
+// /default markup elements
 
-function renderTask (task) {
-    if ('content' in document.createElement('template')) {
-        const taskEl = taskTemplate.content.cloneNode(true);
+// navBar functionality
+    const navButtons = navBar.querySelectorAll('.header__nav-btn');
+    navButtons.forEach(btn => btn.addEventListener('click', navButtonHandler));
+// /navBar functionality
 
-        // components of template
-            const taskContent = taskEl.querySelector('.js--task__content');
-            const taskTitle = taskEl.querySelector('.js--task__title');
-            const taskDesc = taskEl.querySelector('.js--task__desc');
-            const taskDate = taskEl.querySelector('.js--task__date');
-            const taskCheckbox = taskEl.querySelector('.js--task__checkbox');
-        // /components of template
+// toDoList render
+    let _displayRule = 'all';
+    renderAllTasks();
+// /toDoList render
 
-        taskTitle.textContent = task.name;
-        taskDesc.textContent = task.description || '';
-        taskDate.textContent = task.due_date || '';
-        let taskDone = task.done;
+// render functions
+    function renderAllTasks() {
+        if (_displayRule === 'all') db.forEach(renderOneTask);
+        else db.filter(task => task.done.toString() === _displayRule).forEach(renderOneTask);
+    };
 
-        stateCheck(task.due_date, task.done, taskContent, taskCheckbox)
+    function renderOneTask (task) {
+        if ('content' in document.createElement('template')) {
+            const taskTemplateClone = taskTemplate.content.cloneNode(true);
 
-        return tasksBlock.appendChild(taskEl);
-      } else {
-        console.log('tag <template> is not supported by this browser');
-      }
-}
+            // components of template
+                const taskEl = taskTemplateClone.querySelector('.task');
+                const taskTitle = taskTemplateClone.querySelector('.task__body-title');
+                const taskDesc = taskTemplateClone.querySelector('.task__body-desc');
+                const taskDate = taskTemplateClone.querySelector('.task__deadline-date');
+                const taskCheckbox = taskTemplateClone.querySelector('.task__body-checkbox');
+                const taskRemove = taskTemplateClone.querySelector('.task__remove');
+            // /components of template
 
-function stateCheck(date, done, content, checkbox) {
-    if (done === true) {
-        content.classList.add('done');
-        checkbox.setAttribute('checked', 'true');
+            taskTitle.textContent = task.name;
+            taskDesc.textContent = task.description || '';
+            taskDate.textContent = task.due_date || '';
+
+            // functionality on events of components
+                taskCheckbox.addEventListener('change', (e) => checkboxHandler(e, task, taskEl));
+                taskRemove.addEventListener('click', (e) => removeBtnHandler(task, taskEl));
+            // /functionality on events of components
+            stateCheck(task.due_date, task.done, taskEl, taskCheckbox)
+
+            return tasksBlock.appendChild(taskEl);
+        } else {
+            console.log('tag <template> is not supported by this browser');
+        }
     }
-    else if (new Date(date + 'T23:59:59') < new Date()) {
-        content.classList.add('overdue');
+// /render functions
+
+// handlers
+    function navButtonHandler(e) {
+        tasksBlock.innerHTML = '';
+        _displayRule = e.target.value
+        renderAllTasks();
+        navButtons.forEach(btn => btn.classList.remove('is-active'));
+        e.target.classList.add('is-active');
     }
-    else return;
-}
 
-function renderAllTasks(db) {
-    db.forEach(renderTask);
-};
+    function checkboxHandler(e, task, taskEl) {
+        db[db.indexOf(task)].done = e.target.checked;
+        if (_displayRule === 'all') taskEl.replaceWith(renderOneTask(db[db.indexOf(task)]))
+        else {
+            if (db[db.indexOf(task)].done === _displayRule) taskEl.replaceWith(renderOneTask(db[db.indexOf(task)]));
+            else taskEl.remove();
+        }
+    }
 
-renderAllTasks(toDoList);
+    function removeBtnHandler(task, taskEl) {
+        db.splice(db.indexOf(task), 1);
+        console.log(db);
+        return taskEl.remove();
+    }
+// /handlers
+
+// service functions
+    function stateCheck(date, done, content, checkbox) {
+        if (done === true) {
+            content.classList.add('done');
+            checkbox.checked = true;
+        }
+        else if (new Date(date + 'T23:59:59') < new Date()) {
+            content.classList.add('overdue');
+        }
+        else return;
+    }
+// /service functions
