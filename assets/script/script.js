8b409707f0504d6b16df3da28666bf839b3fd23d
InterlinkIntern2 <interlinkintern2@intern-pc.interlink.office>
2022-08-24 18:17:06 +0300

    addForm created

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,9 +4,9 @@
 // /idGenerator
 
 const db = [
-    {id: genId(), name: 'Увімкнути тахометр', description: 'Інакше буде боляче', done: true, due_date: '2022-08-22', list_id: 1},
-    {id: genId(), name: 'Доробити верстку макету', done: false, due_date: '2022-08-20', list_id: 1},
-    {id: genId(), name: 'Піти додому', description: 'Інакше мене закриють у офісі', done: false, due_date: '2022-08-23', list_id: 1},
+    {id: genId(), name: 'Увімкнути тахометр', description: 'Інакше буде боляче', done: true, due_date: new Date('2022-08-22'), list_id: 1},
+    {id: genId(), name: 'Доробити верстку макету', done: false, due_date: new Date('2022-08-20'), list_id: 1},
+    {id: genId(), name: 'Піти додому', description: 'Інакше мене закриють у офісі', done: false, due_date: new Date('2022-08-24'), list_id: 1},
 ];
 
 // default markup elements
@@ -16,9 +16,41 @@ const db = [
 // /default markup elements
 
 // navBar functionality
-    const navButtons = navBar.querySelectorAll('.header__nav-btn');
+    const navButtons = navBar.querySelectorAll('.header__nav-link');
     navButtons.forEach(btn => btn.addEventListener('click', navButtonHandler));
 // /navBar functionality
+
+// modal functionality
+    const modalBg = document.querySelector('.addTask-modal');
+    // open modal
+        const openModalBtn = document.querySelector('.header__btn-open-modal');
+        openModalBtn.addEventListener('click', openModalBtnHandler);
+    // /open modal
+
+    // close modal
+        const modalCloseButtons = modalBg.querySelectorAll('.addTask-modal__close-btn');
+        modalCloseButtons.forEach(btn => btn.addEventListener('click', closeModalBtnHandler));
+
+        modalBg.addEventListener('click', e => {
+            if (e.target.classList.contains('addTask-modal')) closeModalBtnHandler()
+        })
+    // /close modal
+
+    // addTask
+        const taskForm = document.forms['addTask'];
+        taskForm.addEventListener('submit', e => {
+            e.preventDefault();
+            const formData = new FormData(taskForm);
+            const task = Object.fromEntries([...formData.entries(), ['done', false], ['list_id', '1']]);
+            if (!taskFormValidation(task, taskForm)) return;
+
+            taskForm.name.classList.remove('invalid');
+            taskForm.reset();
+            db.push(task)
+            renderOneTask(task);
+        })
+    // /addTask
+// /modal functionality
 
 // toDoList render
     renderAllTasks();
@@ -42,9 +74,12 @@ const db = [
                 const taskRemove = taskTemplateClone.querySelector('.task__remove');
             // /components of template
 
+
+            task.due_date = dateValidation(new Date(task.due_date));
+
             taskTitle.textContent = task.name;
             taskDesc.textContent = task.description || '';
-            taskDate.textContent = task.due_date || '';
+            taskDate.textContent = task.due_date ? task.due_date.toISOString().split('T')[0] : '';
 
             // functionality on events of components
                 taskCheckbox.addEventListener('change', (e) => checkboxHandler(e, task, taskEl));
@@ -55,12 +90,13 @@ const db = [
             return tasksBlock.appendChild(taskEl);
         } else {
             console.log('tag <template> is not supported by this browser');
-        }
-    }
+        };
+    };
 // /render functions
 
 // handlers
     function navButtonHandler(e) {
+
         tasksBlock.innerHTML = '';
         tasksBlock.classList.remove('show-all');
         tasksBlock.classList.remove('show-done');
@@ -70,29 +106,54 @@ const db = [
         renderAllTasks();
         navButtons.forEach(btn => btn.classList.remove('is-active'));
         e.target.classList.add('is-active');
-    }
+    };
 
     function checkboxHandler(e, task, taskEl) {
         db[db.indexOf(task)].done = e.target.checked;
         taskEl.replaceWith(renderOneTask(db[db.indexOf(task)]));
-    }
+    };
 
     function removeBtnHandler(task, taskEl) {
         db.splice(db.indexOf(task), 1);
         console.log(db);
         return taskEl.remove();
-    }
+    };
+
+    function openModalBtnHandler() {
+        modalBg.classList.add('is-active');
+    };
+
+    function closeModalBtnHandler() {
+        modalBg.classList.remove('is-active');
+    };
 // /handlers
 
 // service functions
     function stateCheck(date, done, content, checkbox) {
+        const now = new Date();
+        const today = new Date(now.toISOString().split('T')[0] + 'T00:00:00');
+
         if (done === true) {
             content.classList.add('done');
             checkbox.checked = true;
         }
-        else if (new Date(date + 'T23:59:59') < new Date()) {
+        else if (date < today && date !== '') {
             content.classList.add('overdue');
         }
         else return;
-    }
+    };
+
+    function dateValidation(date) {
+        if (date == 'Invalid Date') date = '';
+        return date;
+    };
+
+    function taskFormValidation(task, form) {
+        if (!task.name) {
+            console.log(form);
+            console.log(form.name);
+            form.name.classList.add('invalid');
+            return false;
+        } else return true;
+    };
 // /service functions
